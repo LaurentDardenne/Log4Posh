812f71432e79fae2e0db9517530a342941d7634c
LaurentDardenne <LaDardenne@Orange.fr>
2017-05-13 14:18:02 +0200

    Initialize tests

---
+++ Test/Log4Posh.Tests.ps1
@@ -0,0 +1,19 @@
+﻿
+Import-Module  "..\Release\Log4Posh\Log4Posh.psd1"
+Write-Warning "Work in progress"
+
+Describe "TODO -Log4Posh" {
+
+ Context "When there is no error" {
+
+  It "basic principle "{
+    $true | Should Be $true
+  }
+ }
+
+ Context "When there error" {
+  It "basic principle "{
+    $true | Should Be $true
+  }
+ }
+}
