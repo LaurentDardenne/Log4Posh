76878bc1f3ddb86cf422e51cbce56ea520852f0e
LaurentDardenne <LaDardenne@Orange.fr>
2017-07-10 10:02:47 +0200

    Add demos for script

---
+++ src/Demos/Demo2ScriptWithLog4Posh.ps1
@@ -0,0 +1,76 @@
+﻿function Un {
+  $DebugLogger.PSDebug("message from function Un")
+  $InfoLogger.PSInfo("message from function Un")
+}
+
+function Deux { 
+  Un
+  $DebugLogger.PSDebug("message from function Deux")
+  $InfoLogger.PSInfo("message from function Deux")
+}
+
+function Trois {
+  Deux
+  $DebugLogger.PSDebug("message from function Trois")
+  $InfoLogger.PSInfo("message from function Trois")
+  
+}
+
+Function Demo {
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Scope="Function")]
+param()
+ #Charge le module prérequis
+$m=Get-Module Log4Posh
+if ($null -eq $M)
+{ Import-Module Log4Posh }
+
+Write-host "`r`nConfigures the default repository" -foreground yellow
+
+ #Get the code of a public function of the module Log4Posh (Prerequisite)
+ # and executes it within the scope of the module
+$InitializeLogging=[scriptblock]::Create("${function:Initialize-Log4NetModule}")
+$RepositoryName = $MyInvocation.ScriptName
+$Params=@{
+  RepositoryName = $RepositoryName
+  XmlConfigPath = "$PSScriptRoot\Demo2Script.Log4Net.Config.xml"
+}
+Write-host "`r`nDefault configuration file" -foreground green
+Write-host "$($params.XmlConfigPath)`r`n"
+&$InitializeLogging @Params
+
+ [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+$lg4n_ScriptName="Demo2ScriptWithLog4Posh"
+$InfoLogger.PSInfo("Change the name of the productor of the log : $lg4n_ScriptName")
+
+Write-host "`r`nShow all created Log4net repositories :"
+[LogManager]::GetAllRepositories()|
+ Select-Object Name,Configured|
+ Format-Table
+ 
+Trois
+
+$sbView={Write-Host "`r`nFor each logger displays the FileAppender file name" -foreground green
+ 
+$params.RepositoryName |
+    #Récupère tous les Appenders nommé 'FileExternal' 
+  Get-Log4NetAppenderFileName |
+   #Affiche le fichier de logs de chaque module 
+  ForEach-Object {Write-Host "`tlog file : $_" -foreground yellow}
+}
+.$sbView
+Write-host "`r`nChanging the location of the main script log file :" -foreground yellow
+$FileName=([System.IO.FileInfo]$RepositoryName).BaseName 
+Switch-AppenderFileName -RepositoryName $RepositoryName FileExternal "C:\temp\$FileName.log"
+Switch-AppenderFileName -RepositoryName $RepositoryName FileInternal "C:\temp\$FileName.log"
+$InfoLogger.PSInfo("Appender FileExternal redirigé")
+
+.$sbView
+
+Write-Host "`r`nStop Log4net. The default repository is no longer configured" -foreground green
+}
+
+if(!$PSScriptRoot)
+{  $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+
+. Demo
+Stop-Log4Net
---
+++ src/Demos/DemoScriptWithLog4Posh.ps1
@@ -0,0 +1,91 @@
+﻿function Un {
+  $DebugLogger.PSDebug("message from function Un")
+  $InfoLogger.PSInfo("message from function Un")
+}
+
+function Deux { 
+  Un
+  $DebugLogger.PSDebug("message from function Deux")
+  $InfoLogger.PSInfo("message from function Deux")
+}
+
+function Trois {
+  Deux
+  $DebugLogger.PSDebug("message from function Trois")
+  $InfoLogger.PSInfo("message from function Trois")
+  
+}
+
+Function Demo {
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Scope="Function")]
+param()
+ #Charge le module prérequis
+$m=Get-Module Log4Posh
+if ($null -eq $M)
+{ $m=Import-Module Log4Posh -PassThru}
+
+Write-host "`r`nConfigures the default repository" -foreground yellow
+Initialize-Log4NetScript -Console All 
+
+$InfoLogger.PSInfo("Name of the productor of the log : $lg4n_ScriptName")
+ [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+$lg4n_ScriptName="DemoScriptWithLog4Posh"
+$InfoLogger.PSInfo("Change the name of the productor of the log : $lg4n_ScriptName")
+
+
+Write-host "`r`nDefault configuration file" -foreground green
+Write-host "`t$($m.ModuleBase)\DefaultLog4Posh.Config.xml"
+
+Write-host "`r`nShow all created Log4net repositories :"
+[LogManager]::GetAllRepositories()|
+ Select-Object Name,Configured|
+ Format-Table
+
+Write-host "`r`nLog information using the loggers variables declared by the 'Initialize-Log4NetScript' function :" -foreground yellow
+Un
+
+Write-host "`r`nChange the layout for the console logger (utc date)"
+$RepositoryName=Get-DefaultRepositoryName
+$Repository=Get-Log4NetRepository -RepositoryName $RepositoryName
+$Console=$Repository.GetAppenders()|Where-Object {$_.Name -eq 'Console'}
+ 
+ #http://logging.apache.org/log4net/release/sdk/?topic=html/T_log4net_Layout_PatternLayout.htm
+$logpattern = "%utcdate{dd MMMM yyyy HH:mm:ss} %-5level - %message%newline"
+$Console.Layout=new-object log4net.Layout.PatternLayout($logpattern)
+$Console.ActivateOptions()
+Trois
+
+Write-host "`r`nDisplays the appenders of each logger declared in the specified modules."
+ Get-DefaultRepositoryName|
+ ForEach-Object { Write-host "`r`n`tLog4NetRepository for the module : $_";$_}|
+ Get-Log4NetRepository|
+ ForEach-Object  {
+   $T=$_.GetAppenders()| Select-Object -ExpandProperty Name
+   $ofs=','
+   Write-Host "`tAppenders : " -foreground green -noNewLine
+   Write-Host "$T" -foreground yellow
+ } 
+# 
+
+Get-DefaultRepositoryName|
+ Get-Log4NetRepository|
+ Get-Log4NetFileAppender -All|
+ Select-Object Name,File
+ 
+ Write-Host "`r`nFor each logger displays the FileAppender file name" -foreground green
+ #déclaré dans les modules précisés.
+   #Noms des modules à interroger
+ Get-DefaultRepositoryName|
+    #Récupère tous les Appenders nommé 'FileExternal' 
+  Get-Log4NetAppenderFileName -External|
+   #Affiche le fichier de logs de chaque module 
+  Get-Content -path {Write-Host "`tContent of script log file : $_" -foreground yellow;$_}
+
+Write-Host "`r`nStop Log4net. The default repository is no longer configured" -foreground green
+}
+
+if(!$PSScriptRoot)
+{  $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+
+. Demo
+Stop-Log4Net
