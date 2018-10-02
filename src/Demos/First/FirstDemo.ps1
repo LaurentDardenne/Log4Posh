3f32bba01ddc7880a4b96e855658619d2e15e89b
LaurentDardenne <LaDardenne@Orange.fr>
2018-10-02 18:54:51 +0200

    Add a simple demo.

---
+++ src/Demos/First/FirstDemo.ps1
@@ -0,0 +1,40 @@
+﻿#Requires -Modules Log4Posh
+
+#Powershell v2
+if (!$PSCommandPath)
+{
+ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+ $m=Get-Module Log4Posh
+ if ($null -eq $M)
+ { $m=Import-Module Log4Posh -PassThru}
+
+ $ScriptName='FirstDemo'
+}
+else
+{ $ScriptName=([System.IO.FileInfo]$PSCommandPath).BaseName }
+
+#$lg4n_ScriptName is added in beginning of a log line (ETS manage this variable)
+$script:lg4n_ScriptName=[log4net.GlobalContext]::Properties["LogJobName"]=$ScriptName
+
+function Log{
+  $DebugLogger.PSDebug("Debug message from function Log. (Send by `$DebugLogger)")
+  $InfoLogger.PSInfo("Information message from function Log.(Send by `$InfoLogger)")
+
+  $DebugLogger.PSError("Error message from function Log.(Send by `$DebugLogger)")
+  $InfoLogger.PSError("Error message from function Log.(Send by `$InfoLogger)")
+}
+
+try {
+ #The script declares the loggers
+ Initialize-Log4Net -RepositoryName $ScriptName -XmlConfigPath "$PSScriptRoot\Log4Net.Config.xml"
+
+ Log
+}
+Finally {
+ Write-warning "Content of the file '$env:Temp\First.log'"
+ Get-Content $env:Temp\First.log
+ Stop-Log4Net
+ "For this demo `$DebugLogger write only to the console."
+ "For this demo `$InfoLogger write only into a file."
+}
---
+++ src/Demos/First/SecondDemo.ps1
@@ -0,0 +1,41 @@
+﻿#Requires -Modules Log4Posh
+
+  $StartDate=[DateTime]::Now
+  $ScriptName=([System.IO.FileInfo]$PSCommandPath).BaseName
+
+   #$lg4n_ScriptName is added in beginning of a log line (ETS manage this variable)
+   #$LogJobName is used for named an appender`s file (Log4net manage this propertie)
+  $Script:lg4n_ScriptName=[log4net.GlobalContext]::Properties["LogJobName"]=$ScriptName
+
+   #Log4net manage this property, it is used for the path of a appender`s file
+  [log4net.GlobalContext]::Properties["ApplicationLogPath"]="C:\Temp"
+   #reads its own properties
+   #$ScriptFileInfo  = Test-ScriptFileInfo -LiteralPath $PSCommandPath
+
+    #Log4net manage this property, this string is added only for the first writing
+  [log4net.GlobalContext]::Properties["Header"]="My Header"
+
+ function Log{
+   $DebugLogger.PSDebug("Debug message from function Log. (Send by `$DebugLogger)")
+   $InfoLogger.PSInfo("Information message from function Log.(Send by `$InfoLogger)")
+
+   $DebugLogger.PSError("Error message from function Log.(Send by `$DebugLogger)")
+   $InfoLogger.PSError("Error message from function Log.(Send by `$InfoLogger)")
+   Throw "Error"
+ }
+
+ try {
+  #The script declares the loggers
+  Initialize-Log4Net -RepositoryName $ScriptName -XmlConfigPath "$PSScriptRoot\Log4Net.Config.xml"
+
+  Log
+ }
+ catch{
+     $InfoLogger.PSFatal("The log content the stack trace of the script",$_)
+     $InfoLogger.Fatal("The log do not content the stack trace of the script",$_.Exception)
+ }
+ Finally {
+     Stop-Log4Net
+     $DebugLogger=$Null
+     $InfoLogger=$Null
+ }
